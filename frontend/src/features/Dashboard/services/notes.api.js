4e36cf00912bc982d0612d611a1f72fa65a4025c
Siddhesh-Tarle05 <tarlesiddhesh@gmail.com>
2026-03-25 23:18:08 +0530

    Used VITE_API_URL for backend instead of localhost

--- frontend/src/features/Dashboard/services/notes.api.js
+++ frontend/src/features/Dashboard/services/notes.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://localhost:3000/api/data',
+  baseURL: `${import.meta.env.VITE_API_URL}/api/data`,
   withCredentials: true,
 });
 
--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api=axios.create({
-     baseURL: "http://localhost:3000/api/auth", 
+     baseURL: `${import.meta.env.VITE_API_URL}/api/auth`, 
     withCredentials:true
 })
 export async function register({email,name,password}) {
