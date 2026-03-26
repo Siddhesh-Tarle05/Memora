c3e3a59aab72f8b257f796505bbeff7e3d7b0be2
Siddhesh-Tarle05 <tarlesiddhesh@gmail.com>
2026-03-26 12:56:55 +0530

    added frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://memora-blue.vercel.app'],
     credentials: true
 }))
 app.use("/api/auth",AuthRouter)
