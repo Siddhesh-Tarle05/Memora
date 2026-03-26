f95877bb75063fc526ba947a3d707c58a274d042
Siddhesh-Tarle05 <tarlesiddhesh@gmail.com>
2026-03-26 14:40:33 +0530

    added header for browser extension

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,8 +2,11 @@ import jwt from "jsonwebtoken";
 import UserModel from "../models/user.model.js";
 
 async function identifyUser(req, res, next) {
+    const authHeader = req.headers.authorization;
     try {
-        const token = req.cookies.token;
+        const token = req.cookies.token || (authHeader && authHeader.startsWith("Bearer ")
+            ? authHeader.split(" ")[1]
+            : null);
 
         if (!token) {
             return res.status(401).json({
@@ -12,10 +15,10 @@ async function identifyUser(req, res, next) {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        let user=await UserModel.findById(decoded.userId)
-        if(!user){
+        let user = await UserModel.findById(decoded.userId)
+        if (!user) {
             return res.status(401).json({
-                message:"user not found"
+                message: "user not found"
             })
         }
         req.user = decoded;
